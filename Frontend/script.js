f60f1d9ae9cde5ccb16c21ba5efd4ae81badef37
Officialdevpro <officialdevpro@gmail.com>
2025-03-11 06:59:02 +0530

    first commit

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("main section");
-    const footerLinks = document.querySelectorAll("footer ul li");
+  const sections = document.querySelectorAll("main section");
+  const footerLinks = document.querySelectorAll("footer ul li");
 
-    footerLinks.forEach((link, index) => {
-        link.addEventListener("click", function () {
-            sections.forEach(section => section.style.display = "none");
-            sections[index].style.display = "flex";
-        });
+  footerLinks.forEach((link, index) => {
+    link.addEventListener("click", function () {
+      sections.forEach((section) => (section.style.display = "none"));
+      sections[index].style.display = "flex";
     });
+  });
+
+  let icon = document.querySelector("svg");
+  icon.addEventListener("click", () => {
+    document.querySelector(".search-bar").classList.toggle("active");
+    if (document.querySelector(".search-bar").classList.contains("active")) {
+      document.querySelector("input").focus();
+    }
+  });
 });
